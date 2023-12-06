7062c7f6086d97d5120f8fe1e835e0ac1f76f431
GrigoriyKruchinin <GrigoriyKruchinin>
2023-12-06 13:03:00 +0100

    added User model and base template

--- geekwriter/users/models.py
+++ geekwriter/users/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
-class User(models.Model):
-    pass
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('author', _('Author')),
+        ('reader', _('Reader')),
+    )
+
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='reader')
+
+    def __str__(self):
+        return self.get_full_name()
+
+    class Meta:
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
